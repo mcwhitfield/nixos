65699c87b9fda2ae2dc0a7658f95a7d7c735f587
Mark Whitfield <mark@whitfield.one>
2024-01-11 13:13:49 -0500

    Add firmware files

--- nixos-modules/rpi.nix
+++ nixos-modules/rpi.nix
@@ -5,24 +5,23 @@
   domain,
   ...
 }: let
-  inherit (builtins) filterSource listToAttrs;
+  inherit (builtins) filter listToAttrs;
   inherit (self.lib) mkEnableOption mkIf mkOption types;
   inherit (self.lib.attrsets) attrByPath selfAndAncestorsEnabled setAttrByPath;
   inherit (self.lib.filesystem) listFilesRecursive;
   inherit (self.lib.strings) removePrefix;
   configKey = [domain "rpi"];
   cfg = attrByPath configKey {} config;
-  firmwareRelease = pkgs.stdenv.mkDerivation {
-    src = filterSource (path: type: baseNameOf path != "Readme.md") pkgs.fetchzip {
-      url = "https://github.com/pftf/RPi4/releases/download/v1.35/RPi4_UEFI_Firmware_v1.35.zip";
-      hash = "sha256-/eeCXVayEfkk0d5OR743djzRgRnCU1I5nJrdUoGmfUk=";
-      stripRoot = false;
-    };
+  firmwareRelease = pkgs.fetchzip {
+    url = "https://github.com/pftf/RPi4/releases/download/v1.35/RPi4_UEFI_Firmware_v1.35.zip";
+    hash = "sha256-/eeCXVayEfkk0d5OR743djzRgRnCU1I5nJrdUoGmfUk=";
+    stripRoot = false;
   };
   firmwareFiles = self.lib.pipe firmwareRelease [
     listFilesRecursive
+    (filter (p: (toString p) != "${firmwareRelease}/Readme.md"))
     (map (p: {
-      name = removePrefix "${firmwareRelease}/" (toString p);
+      name = removePrefix "${firmwareRelease}/" (builtins.unsafeDiscardStringContext p);
       value = p;
     }))
     listToAttrs
@@ -62,7 +61,7 @@ in {
         generic-extlinux-compatible.enable = false;
         systemd-boot = {
           enable = true;
-          # extraFiles = firmwareFiles;
+          extraFiles = firmwareFiles;
         };
       };
       initrd = {
