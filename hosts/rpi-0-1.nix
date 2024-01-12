a8470247826b391ad53f79603af632525597b01b
Mark Whitfield <mark@whitfield.one>
2024-01-11 23:27:48 -0500

    Clean up rpi-0-1 a bit

--- hosts/rpi-0-1.nix
+++ hosts/rpi-0-1.nix
@@ -1,28 +1,15 @@
-{
-  domain,
-  nixosHardware,
-  ...
-}: {
-  imports = [
-    nixosHardware.nixosModules.raspberry-pi-4
-  ];
-
-  config = {
-    networking.hostId = "118bb6f0";
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.generic-extlinux-compatible.enable = false;
-    boot.loader.systemd-boot.enable = true;
-    boot.initrd.network.ssh.hostKeys = [./tmp-host-key];
-    ${domain} = {
-      network.nat.externalInterface = "end0";
-      disko.disk = "/dev/disk/by-id/ata-Samsung_SSD_870_QVO_1TB_S5RRNF0W311551X";
-      rpi = {
-        enable = true;
-        cluster = 0;
-        node = 1;
-      };
-      services.vaultwarden.enable = true;
-      users.mark.enable = true;
+{domain, ...}: {
+  networking.hostId = "118bb6f0";
+  boot.initrd.network.ssh.hostKeys = [./tmp-host-key];
+  ${domain} = {
+    network.nat.externalInterface = "end0";
+    disko.disk = "/dev/disk/by-id/ata-Samsung_SSD_870_QVO_1TB_S5RRNF0W311551X";
+    rpi = {
+      enable = true;
+      cluster = 0;
+      node = 1;
     };
+    services.gitlab.enable = true;
+    users.mark.enable = true;
   };
 }
