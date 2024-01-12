d8851d9e7f183ab2bd25d9be773537fc880b7cbf
Mark Whitfield <mark@whitfield.one>
2024-01-12 08:00:32 -0500

    rpi-0-2 disk label

--- hosts/rpi-0-2.nix
+++ hosts/rpi-0-2.nix
@@ -4,11 +4,11 @@
   ...
 }: {
   imports = [self.nixosModules.rpi self.nixosModules.users-mark];
-  networking.hostId = "118bb6f0";
+  networking.hostId = "233a13d9";
   boot.initrd.network.ssh.hostKeys = [./tmp-host-key];
   ${domain} = {
     network.nat.externalInterface = "end0";
-    disko.disk = "/dev/disk/by-id/ata-Samsung_SSD_870_QVO_1TB_S5RRNF0W311551X";
+    disko.disk = "/dev/disk/by-id/ata-Samsung_SSD_870_QVO_1TB_S5RRNF0W311549R";
     rpi = {
       enable = true;
       cluster = 0;
