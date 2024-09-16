3ce1cee35730d469547aa7301a35c21b3615b7cd
Mark Whitfield <mark@whitfield.one>
2024-09-16 17:08:35 -0400

    2024/09/16

--- modules/default.nix
+++ modules/default.nix
@@ -1,12 +1,29 @@
 {
+  self,
   domain,
   nixosGenerators,
   ...
-}: {
+}: let
+  inherit (self.lib) mkForce;
+  sdCardConf = {...}: {
+    ${domain} = {
+      boot.ssh-decrypt.enable = mkForce false;
+      boot.systemd-boot.enable = mkForce false;
+      disko.enable = mkForce false;
+      persist.enable = mkForce false;
+    };
+  };
+in {
   imports = [
     nixosGenerators.nixosModules.all-formats
   ];
 
-  config.networking.domain = domain;
-  config.system.stateVersion = "23.11";
+  config = {
+    formatConfigs.sd-aarch64 = sdCardConf;
+    formatConfigs.sd-aarch64-installer = sdCardConf;
+    # this is for some reason the slowest step in a build, and not really that important
+    documentation.man.generateCaches = false;
+    networking.domain = domain;
+    system.stateVersion = "23.11";
+  };
 }
